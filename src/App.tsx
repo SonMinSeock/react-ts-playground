18a3c73aca5e8509137e2fab080c1921175ceaf2
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-04-18 16:48:09 +0900

    feat: 전역 스타일, 테마 정의 - styled-reset 이용하여 reset 스타일 적용 - theme.d.ts 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { GlobalStyle } from "./styles/globalStyle";
+
 function App() {
   return (
     <>
+      <GlobalStyle />
       <h1>Vite App</h1>
     </>
   );
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color: ${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor}
+
+  }  
+`;
