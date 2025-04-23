d577a5e0f8c4e5b9391d46fd63e7dccdf5ecb340
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-04-23 20:22:03 +0900

    feat: 코인 정보, 가격 시세 인터페이스 정의

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -10,6 +10,63 @@ interface RouteState {
   name: string;
 }
 
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IQuote {
+  price: number;
+  volume_24h: number;
+  volume_24h_change_24h: number;
+  market_cap: number;
+  market_cap_change_24h: number;
+  percent_change_15m: number;
+  percent_change_30m: number;
+  percent_change_1h: number;
+  percent_change_6h: number;
+  percent_change_12h: number;
+  percent_change_24h: number;
+  percent_change_7d: number;
+  percent_change_30d: number;
+  percent_change_1y: number;
+  ath_price: number;
+  ath_date: string;
+  percent_from_price_ath: number;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: IQuote;
+  };
+}
+
 const Container = styled.div`
   padding: 0 20px;
   max-width: 480px;
@@ -38,8 +95,8 @@ function Coin() {
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
   const [error, setError] = useState<string | null>(null);
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<IInfoData>({});
+  const [priceInfo, setPriceInfo] = useState<IPriceData>({});
 
   useEffect(() => {
     (async () => {
@@ -52,6 +109,7 @@ function Coin() {
         }
 
         const priceData = await priceDataResponse.json();
+        console.log(priceData);
         const infoData = await infoDataResponse.json();
         console.log(infoData);
       } catch (error) {
@@ -71,7 +129,7 @@ function Coin() {
       <Header>
         <Title>{state?.name || "Loading"}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : <span>{}</span>}
+      {loading ? <Loader>Loading...</Loader> : null}
     </Container>
   );
 }
