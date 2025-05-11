2726a0aacf68ed1d227c1eaeca4fa5e5fafdb44e
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-05-11 20:00:37 +0900

    feat: Forms - React Hook Form 소개 - React Hook Form의 useForm 사용해보기 - React Hook Form 라이브러리 이점 중 여러개 입력창 만들어보기

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setTodo] = useState("");
+  const { register, watch } = useForm();
 
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input placeholder="Write a to do" value={toDo} onChange={onChange} />
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("username")} placeholder="Username" />
+        <input {...register("password")} placeholder="Password" />
+        <input {...register("password1")} placeholder="Password1" />
         <button>Add</button>
       </form>
     </div>
