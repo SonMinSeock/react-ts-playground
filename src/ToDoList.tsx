9a25ac503eadf7651c7b39e6f7280249849edc8a
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-05-13 19:25:02 +0900

    feat: Recap, useForm의 setValue 이용하기 - ToDoList 컴포넌트 초기 셋팅 - useForm의 setValue 이용해서 제출 후 입력 값 초기화하기

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,86 +1,22 @@
 import { useForm } from "react-hook-form";
 
-interface IFormData {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError: string;
+interface IForm {
+  toDo: string;
 }
 
 function ToDoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IFormData>();
+  const { register, handleSubmit, setValue } = useForm<IForm>();
 
-  const onValid = (data: IFormData) => {
-    if (data.password !== data.password1) {
-      setError("password1", { message: "Password are not the same." }, { shouldFocus: true });
-    }
-    // setError("extraError", { message: "Server offline." });
+  const handleValid = (data: IForm) => {
+    console.log(`add to do : ${data.toDo}`);
+    setValue("toDo", "");
   };
 
   return (
     <div>
-      <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onValid)}>
-        <input
-          {...register("email", {
-            required: "Email is Required.",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com emails allowed.",
-            },
-          })}
-          placeholder="Email"
-        />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("firstName", {
-            required: "First Name is Required.",
-            validate: {
-              noSon: (data: string) => (data.includes("Son") ? "no Son not allowed." : true),
-              noNiki: (data: string) => (data.includes("Niki") ? "no Niki not allowed." : true),
-            },
-          })}
-          placeholder="First Name"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input {...register("lastName", { required: "Last Name is Required." })} placeholder="Last Name" />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register("username", {
-            required: "Username is Required.",
-            minLength: { value: 10, message: "Your username is too short." },
-          })}
-          placeholder="Username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register("password", {
-            required: "Password is Required.",
-            minLength: {
-              value: 5,
-              message: "Your password is too short.",
-            },
-          })}
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password1", {
-            required: "Password1 is Required.",
-            minLength: { value: 5, message: "Your password1 is too short." },
-          })}
-          placeholder="Password1"
-        />
-        <span>{errors?.password1?.message}</span>
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input {...register("toDo", { required: "Please write a To Do." })} type="text" placeholder="Write a to do" />
         <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
